0bed99647c8f838d0a25e1a95468cb4e7c9c9766
MaxwellW32 <maxwellwedderburn32@gmail.com>
2023-12-09 03:01:45 -0500

    nice

--- app/components/twoColumnHeader/TwoColumnHeader.tsx
+++ app/components/twoColumnHeader/TwoColumnHeader.tsx
@@ -4,25 +4,28 @@ import { CSSProperties } from "react"
 
 export default function TwoColumnHeader(
     {
-        imageOptions = { alt: "image", src: "", styles: {} },
-        headingOptions = { text: "Heading", styles: {} },
-        paragraphOptions = { text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat rerum explicabo voluptatem a? Alias libero dolore nesciunt, aliquam, repudiandae aspernatur nulla in iure dolorum explicabo impedit quaerat, consequatur temporibus.", styles: {} },
+        imageOptions,
+        imageQuality = 1500,
+        headingOptions,
+        paragraphOptions,
         paragraphAndHeadingContStyles,
         mainContStyles,
     }: {
-        imageOptions?: { alt: string, src: string, styles: CSSProperties },
-        headingOptions?: { text: string, styles: CSSProperties },
-        paragraphOptions?: { text: string, styles: CSSProperties },
+        imageOptions: { alt: string, src: string, styles: CSSProperties },
+        headingOptions: { text: string, styles: CSSProperties },
+        paragraphOptions: { text: string, styles: CSSProperties },
+        imageQuality?: number,
         paragraphAndHeadingContStyles?: CSSProperties,
         mainContStyles?: CSSProperties,
     }) {
 
     return (
-        <div className="RU" style={{ padding: "1rem", display: "flex", flexWrap: "wrap", gap: "1rem", ...mainContStyles }}>
-            <Image alt={imageOptions.alt} src={require("@/public/default-image-mountain.jpeg").default.src} width={1000} height={1000} style={{ flex: 1, width: "100%", height: "250px", objectFit: "cover", ...imageOptions.styles }} />
+        <div style={{ padding: "1rem", display: "flex", flexWrap: "wrap", gap: "1rem", ...mainContStyles }}>
+            <Image alt={imageOptions.alt} src={imageOptions.src} width={imageQuality} height={imageQuality} style={{ flex: "1 1 300px", width: "100%", height: "250px", objectFit: "cover", ...imageOptions.styles }} />
 
             <div style={{ flex: "1 1 300px", display: "flex", flexDirection: "column", gap: "1rem", ...paragraphAndHeadingContStyles }}>
                 <h1 className="RU_largeText" style={{ ...headingOptions.styles }}>{headingOptions.text}</h1>
+
                 <p style={{ ...paragraphOptions.styles }}>{paragraphOptions.text}</p>
             </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,36 @@
+import ImageBackground from "./components/imageBackground/ImageBackground";
 import TwoColumnHeader from "./components/twoColumnHeader/TwoColumnHeader";
 
 
 export default function Home() {
   return (
     <main style={{ display: "grid", gap: "1rem" }}>
-      <p style={{ textAlign: "center" }}>Running tests</p>
-      <br />
-
       <p className="signalText">Two Column Header</p>
-      <TwoColumnHeader headingOptions={{ text: "Heading", styles: { color: "blue" } }} imageOptions={{ alt: "", src: "", styles: { height: "300px" } }} />
-      <TwoColumnHeader mainContStyles={{ flexDirection: "row-reverse" }} />
+      <TwoColumnHeader
+        paragraphOptions={{ text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat rerum explicabo voluptatem a? Alias libero dolore nesciunt, aliquam, repudiandae aspernatur nulla in iure dolorum explicabo impedit quaerat, consequatur temporibus.", styles: {} }}
+        imageOptions={{ alt: "image", src: "https://images.pexels.com/photos/417173/pexels-photo-417173.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", styles: { height: "300px" } }}
+        headingOptions={{ text: "Heading", styles: {} }}
+      />
+
+      <p className="signalText">Two Column Header Reversed</p>
+      <TwoColumnHeader
+        mainContStyles={{ flexDirection: "row-reverse" }}
+        paragraphOptions={{ text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nam fugiat rerum explicabo voluptatem a? Alias libero dolore nesciunt, aliquam, repudiandae aspernatur nulla in iure dolorum explicabo impedit quaerat, consequatur temporibus.", styles: {} }}
+        imageOptions={{ alt: "image", src: "https://images.pexels.com/photos/417173/pexels-photo-417173.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2", styles: { height: "300px" } }}
+        headingOptions={{ text: "Heading", styles: {} }}
+      />
+
+      <p className="signalText">Image Background</p>
+      <ImageBackground
+        backgroundImageOptions={{ src: `url(https://images.pexels.com/photos/620337/pexels-photo-620337.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2)`, styles: {} }}
+        backdropStyles={{ backgroundColor: "white", opacity: 0.4 }}
+      >
+        <h1 className='RU_largeText' style={{ textAlign: "center" }}>Image Background</h1>
+        <h1 className='RU_largeText' style={{ textAlign: "center" }}>Image Background</h1>
+        <h1 className='RU_largeText' style={{ textAlign: "center" }}>Image Background</h1>
+        <h1 className='RU_largeText' style={{ textAlign: "center" }}>Image Background</h1>
+        <h1 className='RU_largeText' style={{ textAlign: "center" }}>Image Background</h1>
+      </ImageBackground>
     </main>
   )
 }
